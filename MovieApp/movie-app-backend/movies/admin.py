f9736010357f2a59cfeb46c13dc4535b6a72ad28
Nikolay <2097nikola2097@gmail.com>
2026-04-10 23:19:11 +0500

    Test Home-page
    Добавил 2 модели: movie, category
    Начал тестировать страницу HOME
    Сейчас Ангулар получает данные через DRF и выводит их на страницу.
    Буду дорабатывать страницу как в дизайне

--- MovieApp/movie-app-backend/movies/admin.py
+++ MovieApp/movie-app-backend/movies/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Movie, Category
 
-# Register your models here.
+admin.site.register(Category)
+
+# Продвинутая регистрация для фильмов
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'year', 'author')
+    search_fields = ('title', 'author')
+    list_filter = ('year',)
+    filter_horizontal = ('categories',)
--- MovieApp/movie-app-frontend/src/app/app.routes.ts
+++ MovieApp/movie-app-frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home-component/home-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+
+  // Позже мы добавим сюда другие маршруты, например:
+  // { path: 'login', component: LoginComponent },
+  // { path: 'movies/:id', component: MovieDetailsComponent }
+];
