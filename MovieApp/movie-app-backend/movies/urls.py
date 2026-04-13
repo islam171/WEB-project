cc95086ef839e307cd841c004b29fc7977892323
Islam <ikabden04@gmail.com>
2026-04-13 13:37:48 +0500

    Add actors and add movie detail view in the backend

--- MovieApp/movie-app-backend/movies/urls.py
+++ MovieApp/movie-app-backend/movies/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import MovieListView
+from .views import MovieListView, MovieDetailView, ActorsListView
 
 urlpatterns = [
     path('api/movies/', MovieListView.as_view(), name='movie-list'),
+    path('api/movies/<int:id>', MovieDetailView.as_view(), name='movie-single'),
+    path('api/actors', ActorsListView.as_view(), name='actor-list'),
 ]
--- MovieApp/movie-app-backend/movies/views.py
+++ MovieApp/movie-app-backend/movies/views.py
@@ -1,7 +1,17 @@
 from rest_framework import generics
-from .models import Movie
-from .serializers import MovieSerializer
+from .models import Movie, Actor
+from .serializers import MovieSerializer, ActorSerializer
+
 
 class MovieListView(generics.ListAPIView):
     queryset = Movie.objects.all() # Пока отдаем все фильмы
     serializer_class = MovieSerializer
+
+class MovieDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    lookup_url_kwarg = 'id'
+
+class ActorsListView(generics.ListAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
