6ba0a4b42937b113f40a5483130c371c0abc11aa
Saken Meirzhan <meirzhansaken096@gmail.com>
2026-04-18 17:28:01 +0500

    Sign-Up page

--- MovieApp/movie-app-frontend/src/app/app.routes.ts
+++ MovieApp/movie-app-frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
 import { About } from './pages/about/about';
 import { Catalog } from './pages/catalog/catalog';
+import { Signup } from './pages/signup/signup';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'movies/:id', component: MovieDetailsComponent },
   { path: 'about' , component: About},
-  { path: 'catalog' , component: Catalog}
+  { path: 'catalog' , component: Catalog},
+  { path: 'sign-up' , component: Signup}
 ];
