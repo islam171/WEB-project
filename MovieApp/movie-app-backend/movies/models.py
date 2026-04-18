93a85d34d47646c6ebf6ca9c44f185d08a59ec8b
Nikolay <2097nikola2097@gmail.com>
2026-04-18 16:21:17 +0500

    Adding final model (maybe final)

--- MovieApp/movie-app-backend/movies/models.py
+++ MovieApp/movie-app-backend/movies/models.py
@@ -1,40 +1,90 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db.models import Avg
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
+    # Можно тоже добавить ID из API, если категории грузятся оттуда
+    tmdb_id = models.IntegerField(unique=True, null=True, blank=True)
+
     def __str__(self):
         return self.name
 
+class Actor(models.Model):
+    tmdb_id = models.IntegerField(unique=True, null=True, blank=True) # ID из внешнего API
+    name = models.CharField(max_length=150)
+    desc = models.CharField(max_length=1000)
+    photo = models.URLField(max_length=1000, blank=True, null=True) # Лучше использовать URLField для ссылок на фото из API
+    popularity = models.FloatField(default=0.0) # Популярность из API
+
+    liked_by = models.ManyToManyField(User, related_name='liked_actors', blank=True)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def local_likes(self):
+        return self.liked_by.count()
+
+
 class Movie(models.Model):
+    tmdb_id = models.IntegerField(unique=True, null=True, blank=True) # ID из внешнего API
     title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255) # Режиссер
-    year = models.IntegerField()
+    author = models.CharField(max_length=255, blank=True, null=True) # Режиссер
+    year = models.IntegerField(blank=True, null=True)
+    duration = models.CharField(max_length=50, blank=True, null=True)
 
-    # Поля для дизайна
-    duration = models.CharField(max_length=50, blank=True, null=True) # "2h 11m"
-    likes = models.IntegerField(default=0)
-    rating = models.CharField(max_length=10, default="0/10") # "10/10"
+    # Рейтинг и популярность из внешнего API
+    tmdb_rating = models.FloatField(default=0.0)
+
     short_description = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
 
-    # Связь с актерами
-    actors = models.ManyToManyField('Actor', related_name='movies')
-
+    # Постеры из API (TMDB отдает просто пути, так что URLField отлично подойдет)
     poster = models.URLField(blank=True, null=True)
     backdrop = models.URLField(blank=True, null=True)
-
-    description = models.TextField(blank=True, null=True)
     videoUrl = models.URLField(blank=True, null=True)
+
+    # Связи
+    actors = models.ManyToManyField('Actor', related_name='movies')
     categories = models.ManyToManyField(Category, related_name='movies')
+    liked_by = models.ManyToManyField(User, related_name='liked_movies', blank=True)
 
     def __str__(self):
         return f"{self.title} ({self.year})"
 
-class Actor(models.Model):
-    name = models.CharField(max_length=150)
-    desc = models.CharField(max_length=1000)
-    photo = models.CharField(max_length=1000, blank=True, null=True)
-    popularity = models.FloatField(default=0.0)
+    @property
+    def local_likes(self):
+        return self.liked_by.count()
+
+    @property
+    def local_rating(self):
+        # Локальный рейтинг от пользователей вашего сайта
+        avg = self.reviews.aggregate(Avg('rating'))['rating__avg']
+        if avg is not None:
+            return round(avg, 1)
+        return 0.0
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='reviews')
+    text = models.TextField(blank=True, null=True)
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)], default=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'movie')
 
     def __str__(self):
-        return self.name
+        return f"Review by {self.user.username} on {self.movie.title}"
+
+
+class Wishlist(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='wishlist')
+    movies = models.ManyToManyField(Movie, related_name='in_wishlists', blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Wishlist"
