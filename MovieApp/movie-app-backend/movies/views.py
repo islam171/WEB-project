0d9d62c2e680bcfd2b0122a99c8c79931fe9eb10
Saken Meirzhan <meirzhansaken096@gmail.com>
2026-04-19 14:33:34 +0500

    added footer sign-in/up pages

--- MovieApp/movie-app-backend/movies/views.py
+++ MovieApp/movie-app-backend/movies/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics
 from .models import Movie, Actor
 from .serializers import MovieSerializer, ActorSerializer
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 class MovieListView(generics.ListAPIView):
     queryset = Movie.objects.all() # Пока отдаем все фильмы
